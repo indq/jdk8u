cdbc264b31172d9e358ceb092d5311adb19d39f0
kb1000 <kaeptmblaubaer1000@gmail.com>
2020-05-05 11:31:04 -0400

    appveyor.yml

---
+++ .appveyor/windows-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+export TMP=C:\\Temp TEMP=C:\\Temp PATH="/bin:$PATH"
+cd /cygdrive/c/projects
+wget http://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.gz
+tar -xzf freetype-2.10.1.tar.gz
+cd /cygdrive/c/projects/jdk8u
+git submodule update --init
+bash ./configure --with-target-bits=64 --with-toolchain-version=2010 --with-freetype-src=/cygdrive/c/projects/freetype-2.10.1 --with-debug-level=release
+make images
